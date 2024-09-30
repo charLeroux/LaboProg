885593ad345c609264a1b578dceb4863628008ef
CharlesLeroux <202045086@edu.clg.qc.ca>
2024-09-30 11:34:32 -0400

    Final Commit

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 // Charles Leroux
 // 16/09/24
 // description : Page web permettant de vérifié l'état des modules 
+const { on } = require('events');
+const e = require('express');
 var express = require('express')
 var app = express();
 var http = require('http');
 app.set('view engine', 'ejs');
 var mqtt = require('mqtt');
+var nbModule = 0;
+var isModule = "";
 
 var client  = mqtt.connect('mqtt://127.0.0.1:1883');
 client.subscribe('MODULE/#');
-var id1 = 0;
-var id2 = 0;
-var id3 = 0;
-var id4 = 0;
-var id5 = 0;
-var id6 = 0;
+let id = [
+    id1 = 0,
+    id2 = 0,
+    id3 = 0,
+    id4 = 0,
+    id5 = 0,
+    id6 = 0
+    ] 
+    
+
 
 app.get('/',function(req,res)
 {
@@ -41,76 +49,71 @@ client.publish('MODULE', 'le serveur js vous dit bonjour');
 client.on('message', function (topic, message) {
     console.log(topic.toString());
     console.log(message.toString());
+    if(topic.toString().split("/")[0] == "MODULE")
+        {
+           
+            if(topic.toString().split("/")[1]>= 1 && topic.toString().split("/")[1] <=6 )
+            {
+                nbModule = topic.toString().split("/")[1];
+                if(message == "on")
+                    {
+                        
+                        id[nbModule-1] = 1;
+                    }
+                else if(message == "off")
+                    {
+                        id[nbModule-1] = 0;
+                    }
+                    console.log(id);
+            }
+            else
+            {
+                console.log("module non trouvable")
+            }
+             
+        }
+     
   });
   
 
 app.get('/module/:varModule',function(req,res)
 {
-    if(req.params.varModule>= 1 && req.params.varModule <= 6)
+    for(var i = 0; i <= 5 ; i++)
         {
-            if(req.params.varModule == 1)
+            if(req.params.varModule>= 1 && req.params.varModule <= 6)
                 {
-                    if(id1 == 0)
-                        id1 = 1;
-                    else
-                        id1 = 0;
+                    if(req.params.varModule == i)
+                        {
+                            if(id[i-1] == 1)
+                                id[i-1] = 0;
+                            else
+                            {
+                                id[i-1] = 1;
+                            }
+                            res.render('./page/module',{valModule: req.params.varModule, module1 : id[0], module2 : id[1], module3 : id[2], module4 : id[3], module5 : id[4], module6 : id[5]});
+                        }
+                       
                 }
-            if(req.params.varModule == 2)
+                else
                 {
-                    if(id2 == 0)
-                        id2 = 1;
-                        else
-                        id2 = 0;
+                    res.render('./page/module',{valModule: "MODULE INTROUVALE"});
                 }
-            if(req.params.varModule == 3)
-                {
-                     if(id3 == 0)
-                        id3 = 1;
-                     else
-                        id3 = 0;
-                }
-                if(req.params.varModule == 4)
-                {
-                    if(id4 == 0)
-                        id4 = 1;
-                    else
-                        id4 = 0;
-                }
-                if(req.params.varModule == 5)
-                    {
-                        if(id5 == 0)
-                            id5 = 1;
-                        else
-                            id5 = 0;
-                    }
-                if(req.params.varModule == 6)
-                    {
-                        if(id6 == 0)
-                            id6 = 1;
-                        else
-                            id6 = 0;
-                    }    
-            res.render('./page/module',{valModule: req.params.varModule, module1 : id1, module2 : id2, module3 : id3, module4 : id4, module5 : id5, module6 : id6});
-        }
-        else
-        {
-            res.render('./page/module',{valModule: "MODULE INTROUVALE"});
         }
 });
 
 app.get('/controle',function (req, res)
 {
-    res.render('./page/controle', { controle1 : id1, controle2 : id2, controle3 : id3, controle4 : id4, controle5 : id5, controle6 : id6 }); 
+    res.render('./page/controle', { controle1 : id[0], controle2 : id[1], controle3 : id[2], controle4 : id[3], controle5 : id[4], controle6 : id[5] }); 
 });
 
 app.get('/reset',function (req, res)
 {
-    id1 = 0;
-    id2 = 0;
-    id3 = 0;
-    id4 = 0;
-    id5 = 0;
-    id6 = 0;
+   id[0] = 0;
+   id[1] = 0;
+   id[2] = 0;
+   id[3] = 0;
+   id[4] = 0;
+   id[5] = 0;
 
     res.render('./page/reset'); 
 });
